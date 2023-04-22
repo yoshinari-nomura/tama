5d5b6c56875fcf38b96e79499e4bfabe6c902cec
Yoshinari Nomura <nom@nomlab.org>
2023-04-23 02:08:37 +0900

    Add Assignment#teaching_assistant_name

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,4 +3,8 @@ class Assignment < ApplicationRecord
   validates :course, presence: true
   belongs_to :course
   belongs_to :teaching_assistant, optional: true
+
+  def teaching_assistant_name
+    self.teaching_assistant || "unspecified-#{self.id}"
+  end
 end
