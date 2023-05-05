a9d24483e8db8ca9360bb3852409af7a6846d656
Yoshinari Nomura <nom@nomlab.org>
2023-05-06 00:05:49 +0900

    Add nested routing to assignments and work_hours under courses

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :work_hours
   resources :assignments
   resources :teaching_assistants
-  resources :courses
+  resources :courses, shallow: true do
+    resources :assignments
+    resources :work_hours
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
