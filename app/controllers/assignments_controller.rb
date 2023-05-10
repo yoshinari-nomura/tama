84b1c9b7b976397abf0888e9d4ba514fb4e6e97e
Yoshinari Nomura <nom@nomlab.org>
2023-05-10 23:25:49 +0900

    Add CRUD for assignments using Turbo frame/stream

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,6 +1,6 @@
 class AssignmentsController < ApplicationController
   before_action :set_assignment, only: %i[ show edit update destroy ]
-  before_action :set_courses, only: %i[ new edit update ]
+  before_action :set_courses_and_teaching_assistants, only: %i[ new create edit update ]
 
   # GET /assignments or /assignments.json
   def index
@@ -14,6 +14,7 @@ class AssignmentsController < ApplicationController
   # GET /assignments/new
   def new
     @assignment = Assignment.new
+    @assignment.course_id = params[:course_id]
   end
 
   # GET /assignments/1/edit
@@ -28,9 +29,17 @@ class AssignmentsController < ApplicationController
       if @assignment.save
         format.html { redirect_to assignment_url(@assignment), notice: "Assignment was successfully created." }
         format.json { render :show, status: :created, location: @assignment }
+
+        # pass notice to create.turbo_stream.erb
+        flash.now.notice = "Assignment was successfully created."
+        format.turbo_stream {} # will be rendered by create.turbo_stream.erb
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @assignment.errors, status: :unprocessable_entity }
+
+        # In case error, we do not return turbo stream.
+        # HTML render new (_form) will work.
+        # Validation will be shown inline, no need to use flash.
       end
     end
   end
@@ -41,9 +50,17 @@ class AssignmentsController < ApplicationController
       if @assignment.update(assignment_params)
         format.html { redirect_to assignment_url(@assignment), notice: "Assignment was successfully updated." }
         format.json { render :show, status: :ok, location: @assignment }
+
+        # pass notice to update.turbo_stream.erb
+        flash.now.notice = "Assignment assistant was successfully updated."
+        format.turbo_stream {} # will be rendered by update.turbo_stream.erb
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @assignment.errors, status: :unprocessable_entity }
+
+        # In case error, we do not return turbo stream.
+        # HTML render edit (_form) will work.
+        # Validation will be shown inline, no need to use flash.
       end
     end
   end
@@ -55,6 +72,10 @@ class AssignmentsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to assignments_url, notice: "Assignment was successfully destroyed." }
       format.json { head :no_content }
+
+      # pass notice to destroy.turbo_stream.erb
+      flash.now.notice = "Assignment was successfully destroyed."
+      format.turbo_stream {} # will be rendered by destroy.turbo_stream.erb
     end
   end
 
@@ -64,8 +85,9 @@ class AssignmentsController < ApplicationController
       @assignment = Assignment.find(params[:id])
     end
 
-    def set_courses
+    def set_courses_and_teaching_assistants
       @courses = Course.all
+      @teaching_assistants = TeachingAssistant.all
     end
 
     # Only allow a list of trusted parameters through.
